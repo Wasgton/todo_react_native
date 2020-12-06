25772be7b16eb7f6f838696664525d15f4fc84ce
wasgton <wasgton.it@gmail.com>
2020-12-06 13:59:09 -0300

    Crud connectando com API

--- App.js
+++ App.js
@@ -1,68 +1,121 @@
-import React,{useState} from 'react';
-import {Text, TextInput, View, StatusBar, TouchableOpacity, Pressable, ScrollView, SafeAreaView} from 'react-native';
+import React,{useState, useEffect} from 'react';
+import {Text, TextInput, View, StatusBar, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
 import styles from './styles';
-import {Button} from "react-native-web";
+import axios from 'axios';
 
 export default function App() {
 
   const [newTask,setNewTask] = useState('');
   const [tasks, setTasks] = useState([]);
+  const instance = axios.create({
+        baseURL: 'http://127.0.0.1:8000/api/',
+  });
+
+    useEffect(()=>{
+        getTasksDb();
+    },[true]);
 
     function handleNewTasks(){
       if(newTask.length>0){
-          let task = {};
-          task.id = Math.floor(Math.random() * new Date());
-          task.task = newTask;
-
-          setTasks([...tasks,task]);
+          setTaskDB(newTask);
           setNewTask('');
       }
     }
 
     function deleteTask(id){
-      let arrTasks = tasks.filter(function(elem,index,array){
-        return elem.id !== id;
-      });
-      setTasks(arrTasks);
+        if(deleteTaskDB(id)){
+            let arrTasks = tasks.filter(function(elem,index,array){
+                return elem.id !== id;
+            });
+
+            setTasks(arrTasks);
+        }
     }
+
+    const getTasksDb = async () =>{
+        await instance .get('task/')
+            .then(function(response){
+                setTasks(response.data);
+                return response;
+            })
+            .catch(function(error){
+                console.log(error);
+                return false;
+            }).then(function () {
+
+            });
+    }
+
+    const setTaskDB = async (task) =>{
+        await instance .post('task/',{data:task})
+            .then(function (response){
+                if(response.statusText == 'Created'){
+                    setTasks([...tasks,response.data]);
+                }
+            })
+            .catch(function (error){
+                console.log(error);
+                return false;
+            })
+            .then(function (){
+
+            })
+    }
+
+    const deleteTaskDB = async (id) =>{
+        await instance.delete('task/'+id)
+            .then(function (response){
+                if(response.statusText == 'Created'){
+                    return true;
+                }
+            })
+            .catch(function (error){
+                console.log(error);
+                return false;
+            })
+            .then(function (){
+
+            })
+    }
+
 
   return (
       <View style={styles.mainContainer}>
-        <View style={styles.topContainer}>
-          <Text style={styles.title}>Tarefas</Text>
-        </View>
-        <View style={styles.formContainer}>
-          <View style={styles.inputContainer}>
-            <TextInput onChangeText={text=> setNewTask(text)}
-                       style={styles.input}
-                       value={newTask}
-                       placeholder="Type your task"
-            />
+          <View style={styles.topContainer}>
+              <Text style={styles.title}>Tarefas</Text>
           </View>
-          <View style={styles.btnContainer}>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={()=>{handleNewTasks()}}
-            >
-              <Text style={styles.buttonText}>Enter</Text>
-            </TouchableOpacity>
+          <View style={styles.formContainer}>
+              <View style={styles.inputContainer}>
+                  <TextInput onChangeText={text=> setNewTask(text)}
+                             style={styles.input}
+                             value={newTask}
+                             placeholder="Type your task"
+                  />
+              </View>
+              <View style={styles.btnContainer}>
+                  <TouchableOpacity
+                      style={styles.button}
+                      onPress={()=>{handleNewTasks()}}
+                  >
+                      <Text style={styles.buttonText}>Enter</Text>
+                  </TouchableOpacity>
+              </View>
           </View>
-        </View>
-        <View style={styles.taskContainer}>
-            <SafeAreaView>
-                <ScrollView>
-                  {tasks.map(task=>(
-                      <View style={styles.listItem} key={task.id} >
-                          <Text style={styles.task}>{task.task}</Text>
-                          <TouchableOpacity style={styles.deleteButton} onPress={()=>deleteTask(task.id)} >
-                              <Text style={styles.deleteButtonText}>Delete</Text>
-                          </TouchableOpacity>
-                      </View>
-                  ))}
-                </ScrollView>
-            </SafeAreaView>
-        </View>
-        <StatusBar backgroundColor="black" StatusBarStyle="dark-content"/>
+          <View style={styles.taskContainer}>
+              <SafeAreaView>
+                  <ScrollView>
+                      {tasks.map(task=>(
+                          <View style={styles.listItem} key={task.id} >
+                              <Text style={styles.task}>{task.description}</Text>
+                              <TouchableOpacity style={styles.deleteButton} onPress={()=>deleteTask(task.id)} >
+                                  <Text style={styles.deleteButtonText}>Delete</Text>
+                              </TouchableOpacity>
+                          </View>
+                      ))}
+                  </ScrollView>
+              </SafeAreaView>
+          </View>
+          <StatusBar backgroundColor="black" StatusBarStyle="dark-content"/>
       </View>
   );
 }
---
+++ apiFunctions.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const api = axios.create({
+   baseURL:'http://127.0.0.1:8000/api/'
+});
+//
+// export default api;
+//
